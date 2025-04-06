94210817b18e562840bbc5be465e1188d85ebd20
윤종환 <yunjonghwan99@gmail.com>
2025-04-06 22:05:48 +0900

    데이터 카테고리 별로 나눠서 담아 봄

--- item.js
+++ item.js
@@ -31,10 +31,15 @@
 
 // }
 
-const ITEMSARR = []
+const ITEMSARR = {
+  block : [],
+  backGround : [],
+}
+
 const ALPHABET = [
   'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ];
+
 const CATEGORY = {
   block : 'Foreground',
   backGround : 'Background',
--- urlCrawler.js
+++ urlCrawler.js
@@ -14,66 +14,73 @@ async function getImage(url) {
   });
 }
 
-async function getUrl() {
-  const res = await axios.get('https://growtopia.fandom.com/wiki/Category:Foreground_Blocks?from=A');
-  const $ = cheerio.load(res.data);
-  const urlList = $('.category-page__member-link');
-  Object.values(urlList).forEach(async (data) => {
-    try {
-      const url = data.attribs.href;
-      const name = data.children[0].data;
-      const image = await getImage(url);
-      const item = new Items(name, url, image);
-      itemsArr.push(item);
-    } catch (error) {
-      return null
-    }
-  });
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(itemsArr);
-    }, 2000)
-  });
-}
+// async function getUrl() {
+//   const res = await axios.get('https://growtopia.fandom.com/wiki/Category:Foreground_Blocks?from=A');
+//   const $ = cheerio.load(res.data);
+//   const urlList = $('.category-page__member-link');
+//   Object.values(urlList).forEach(async (data) => {
+//     try {
+//       const url = data.attribs.href;
+//       const name = data.children[0].data;
+//       const image = await getImage(url);
+//       const item = new Items(name, url, image);
+//       itemsArr.push(item);
+//     } catch (error) {
+//       return null
+//     }
+//   });
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(itemsArr);
+//     }, 2000)
+//   });
+// }
 
-async function test() {
-  let arr = []
-  arr = await getUrl();
-  console.log(itemsArr);
-  console.log(itemsArr.length);
-}
+// async function test() {
+//   let arr = []
+//   arr = await getUrl();
+//   console.log(itemsArr);
+//   console.log(itemsArr.length);
+// }
 
 async function getApi(category, to, from) {
   try {
     const res = await axios.get(`https://growtopia.fandom.com/api.php?action=query&format=json&prop=&titles=&generator=categorymembers&formatversion=2&gcmtitle=Category%3A${category}%20Blocks&gcmlimit=500&gcmstartsortkeyprefix=${to}&gcmendsortkeyprefix=${from}`);
     const dataArray = res.data.query.pages;
+    // console.log(dataArray);
     Array.from(dataArray).forEach((data) => {
+      // console.log(data);
       const itemsObj = {}
       itemsObj.title = data.title;
-      ITEMSARR.push(itemsObj);
+      itemsObj.category = category;
+      if(category === CATEGORY.block) {
+        ITEMSARR.block.push(itemsObj);
+      }else {
+        ITEMSARR.backGround.push(itemsObj);
+      }
+      console.log(ITEMSARR);
     })
   } catch (error) {
-    new Error(error);
+    console.log(new Error(error));
   }
-
-  console.log(ITEMSARR);
-  return new Promise((resolve) => {
-    resolve(ITEMSARR);
-  })
+  // console.log(ITEMSARR);
+  // return new Promise((resolve) => {
+  //   resolve(ITEMSARR);
+  // })
 }
 
-// getApi(CATEGORY.backGround, "Q", "R");
+getApi(CATEGORY.backGround, "A", "B");
+getApi(CATEGORY.block, "A", "B");
 
+// async function getData() {
+//   let arr;
+//   Object.values(CATEGORY).forEach(async (category) => {
+//     for (let i = 0; i < ALPHABET.length; i++) {
+//       console.log(ALPHABET[i + 1]);
+//       arr = await getApi(category, ALPHABET[i], ALPHABET[i + 1]);
+//     }
+//     console.log(ITEMSARR)
+//   })
+// }
 
-async function getData() {
-  let arr;
-  Object.values(CATEGORY).forEach(async (category) => {
-    for (let i = 0; i < ALPHABET.length; i++) {
-      console.log(ALPHABET[i + 1]);
-      arr = await getApi(CATEGORY.backGround, ALPHABET[i], ALPHABET[i + 1]);
-    }
-    console.log(ITEMSARR)
-  })
-}
-
-getData();
+// getData();
