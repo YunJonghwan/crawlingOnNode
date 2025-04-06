1b574a6d2ce54b991dfdb9e40cffdebbda6402c1
윤종환 <yunjonghwan99@gmail.com>
2025-04-06 20:51:04 +0900

    알파벳을 이용해서 값을 가져오기 위해 배열 추가

--- item.js
+++ item.js
@@ -32,5 +32,7 @@ class items {
 }
 
 const itemsArr = []
-
-export { items, itemsArr };
+const alphabet = [
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+];
+export { items, itemsArr, alphabet };
