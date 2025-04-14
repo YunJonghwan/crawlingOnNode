d703944fbd427f84bc09cd8ee219d5156f321a27
윤종환 <yunjonghwan99@gmail.com>
2025-04-14 21:52:54 +0900

    all일 경우 중간에 에러가 발생하게 되면 멈추기 때문에 allSettled로 에러가 발생해도 지나가도록 설정함 하지만 데이터를 다 받아오고 있지 않음

--- promisePractice.js
+++ promisePractice.js
@@ -5,7 +5,7 @@ async function fetchByLetter(category, to, from) {
   try {
     const res = await axios.get(`https://growtopia.fandom.com/api.php?action=query&format=json&prop=&titles=&generator=categorymembers&formatversion=2&gcmtitle=Category%3A${category}%20Blocks&gcmlimit=500&gcmstartsortkeyprefix=${to}&gcmendsortkeyprefix=${from}`);
     const dataArray = res.data.query.pages;
-    console.log(dataArray);
+    ITEMSARR.push(dataArray);
   } catch (error) {
     console.log(error);
   }
@@ -13,7 +13,7 @@ async function fetchByLetter(category, to, from) {
 
 async function fetchAllAlphabet() {
   const promise = ALPHABET.slice(0, -1).map((alphabet, index) => fetchByLetter("Foreground", alphabet, alphabet[index + 1]));
-  const result = await Promise.all(promise);
+  const result = await Promise.allSettled(promise);
   console.log(result);
 }
 
