46caf61e1fdc36678105cec3b6fc2ccf389c246f
윤종환 <yunjonghwan99@gmail.com>
2025-04-14 21:47:02 +0900

    기존의 fetch 문을 promise 형태와 맞추도록 수정

--- promisePractice.js
+++ promisePractice.js
@@ -1,7 +1,19 @@
-import { ALPHABET } from './item.js';
+import axios from 'axios';
+import { ALPHABET, ITEMSARR } from './item.js';
+
+async function fetchByLetter(category, to, from) {
+  try {
+    const res = await axios.get(`https://growtopia.fandom.com/api.php?action=query&format=json&prop=&titles=&generator=categorymembers&formatversion=2&gcmtitle=Category%3A${category}%20Blocks&gcmlimit=500&gcmstartsortkeyprefix=${to}&gcmendsortkeyprefix=${from}`);
+    const dataArray = res.data.query.pages;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 async function fetchAllAlphabet() {
-  ALPHABET.slice(0, -1).map((alphabet, index) => console.log(alphabet, ALPHABET[index + 1]));
+  const promise = ALPHABET.slice(0, -1).map((alphabet, index) => fetchByLetter("block", alphabet, alphabet[index + 1]));
+  const result = await Promise.all(promise);
+  console.log(result);
 }
 
 fetchAllAlphabet();
