c7c99714d31c63de93aac60935dbb0453e83a5ac
윤종환 <yunjonghwan99@gmail.com>
2025-04-14 21:49:48 +0900

    category가 block이라서 데이터를 전부 가져오지 못하고 있었음 category를 변경하고 console로 로그를 확인함

--- promisePractice.js
+++ promisePractice.js
@@ -5,13 +5,14 @@ async function fetchByLetter(category, to, from) {
   try {
     const res = await axios.get(`https://growtopia.fandom.com/api.php?action=query&format=json&prop=&titles=&generator=categorymembers&formatversion=2&gcmtitle=Category%3A${category}%20Blocks&gcmlimit=500&gcmstartsortkeyprefix=${to}&gcmendsortkeyprefix=${from}`);
     const dataArray = res.data.query.pages;
+    console.log(dataArray);
   } catch (error) {
     console.log(error);
   }
 }
 
 async function fetchAllAlphabet() {
-  const promise = ALPHABET.slice(0, -1).map((alphabet, index) => fetchByLetter("block", alphabet, alphabet[index + 1]));
+  const promise = ALPHABET.slice(0, -1).map((alphabet, index) => fetchByLetter("Foreground", alphabet, alphabet[index + 1]));
   const result = await Promise.all(promise);
   console.log(result);
 }
