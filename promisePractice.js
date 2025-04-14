41f23e1b6f36a1a96d3a7cc4c76485b71adc15b0
윤종환 <yunjonghwan99@gmail.com>
2025-04-14 22:17:03 +0900

    promise.allSettled를 사용해서 데이터를 가져오도록 했지만 왜인지 가져온 데이터가 양이 너무 많다 블럭의 경우 많아도 2400개인데 약 10000개가 나오고 배경도 1000개가 안될텐데 3150개가 나오니 어디서 꼬인건지

--- promisePractice.js
+++ promisePractice.js
@@ -1,20 +1,34 @@
 import axios from 'axios';
-import { ALPHABET, ITEMSARR } from './item.js';
+import { ALPHABET, ITEMSARR, CATEGORY } from './item copy.js';
 
 async function fetchByLetter(category, to, from) {
   try {
     const res = await axios.get(`https://growtopia.fandom.com/api.php?action=query&format=json&prop=&titles=&generator=categorymembers&formatversion=2&gcmtitle=Category%3A${category}%20Blocks&gcmlimit=500&gcmstartsortkeyprefix=${to}&gcmendsortkeyprefix=${from}`);
     const dataArray = res.data.query.pages;
-    ITEMSARR.push(dataArray);
+    dataArray.forEach(async (data) => {
+      const itemsObj = {}
+      itemsObj.title = data.title;
+      itemsObj.category = category;
+      if(category === CATEGORY.block) {
+        ITEMSARR.block.push(itemsObj);
+      }else {
+        ITEMSARR.backGround.push(itemsObj);
+      }
+    })
   } catch (error) {
     console.log(error);
   }
 }
 
 async function fetchAllAlphabet() {
-  const promise = ALPHABET.slice(0, -1).map((alphabet, index) => fetchByLetter("Foreground", alphabet, alphabet[index + 1]));
-  const result = await Promise.allSettled(promise);
-  console.log(result);
+  // const block = ALPHABET.slice(0, -1).map((alphabet, index) => fetchByLetter(CATEGORY.block, alphabet, alphabet[index + 1]));
+  const backGround = ALPHABET.slice(0, -1).map((alphabet, index) => fetchByLetter(CATEGORY.backGround, alphabet, alphabet[index + 1]));
+
+  await Promise.allSettled(backGround);
+  console.log(ITEMSARR.block);
+  console.log(ITEMSARR.backGround);
+  console.log(ITEMSARR.block.length);
+  console.log(ITEMSARR.backGround.length);
 }
 
 fetchAllAlphabet();
