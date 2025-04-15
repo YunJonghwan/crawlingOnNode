2e5cc86867186700ed58c8e5f13053c0a1737955
윤종환 <yunjonghwan99@gmail.com>
2025-04-15 18:53:34 +0900

    promisePractice를 진행하게 되면 값을 가지고는 오지만 반복문에서 꼬였느지 어디서 꼬였는지 하나하나 확인해 보기 위해서 Test파일을 만듦

---
+++ promisePracticeTest.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import { ALPHABET, ITEMSARR, CATEGORY } from './item copy.js';
+
+async function testFetch(category, to, from) {
+  try {
+    const res = await axios.get(`https://growtopia.fandom.com/api.php?action=query&format=json&prop=&titles=&generator=categorymembers&formatversion=2&gcmtitle=Category%3A${category}%20Blocks&gcmlimit=500&gcmstartsortkeyprefix=${to}&gcmendsortkeyprefix=${from}`);
+    const dataArray = res.data.query.pages;
+    dataArray.forEach((data) => {
+      const itemsObj = {}
+      itemsObj.title = data.title;
+      itemsObj.category = category;
+      if(category === CATEGORY.block) {
+        ITEMSARR.block.push(itemsObj);
+      }else {
+        ITEMSARR.backGround.push(itemsObj);
+      }
+    })
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+async function test() {
+  const fetchTest = await testFetch(CATEGORY.block, "A", "B");
+  await Promise.allSettled(fetchTest)
+  console.log(ITEMSARR);
+  console.log(ITEMSARR.block.length);
+}
+
+test();
